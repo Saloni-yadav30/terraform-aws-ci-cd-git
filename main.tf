f9ab4522da90dcce89c33a6df70dcb014490a9c9
EC2 Default User <ec2-user@ip-172-31-26-16.ec2.internal>
2024-11-11 07:44:50 +0000

    Update main.tf with new changes

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
-provider "aws"{
-region = "us-east-1"
-}
-resource "aws_instance" "example"{
-ami = ami-0984f4b9e98be44bf
-instance_type = "t2.micro"  
-}
-tags = {
- Name = "MyEC2Instance"
- }
-}
-resource "aws_s3_bucket" "my_bucket" { 
-bucket = "my-unique-bucket-name" # Replace and Use a globally unique bucket name 
-acl = "private" 
 
-} 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0984f4b9e98be44bf"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name        = "MyExampleInstance"
+    Environment = "Production"
+  }
+resource "aws_s3_bucket" "my_bucket"{
+bucket = "saloniyadav301001"
+acl = "private"
+}
+
+
